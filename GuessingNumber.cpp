dd8b06e165fd8ab5bbb6c8f1bda4a44292a96d43
Osta <41627179+MrOsta@users.noreply.github.com>
2023-02-17 19:03:32 +0300

    Create GuessingNumber.cpp

---
+++ GuessingNumber.cpp
@@ -0,0 +1,33 @@
+// Not really the best way to solve such a question, I've to admit. I am still a newbie.
+// I am not "FOR SURE" accepting my current level, but "Allah is always with those who are truly patient"
+// i don't have to be in a hurry to do Great Things in One DAY:)
+// PATIENCE IS POWER.
+
+#include <iostream>
+using namespace std;
+
+int main (){
+
+
+	int first =  1; 
+	int last  =  100;
+	int count = 0;
+	// first == last, then it must be working.
+	while(first != last) {
+		int mid = (first + last ) / 2 ; 
+		string ans = "" ;
+		cout << "Is the number greater than, " << mid ;
+		cin >> ans;
+		if(ans == "y") {
+			// (mid, last] possible range  " half-interval "
+			first = mid +1;
+		} else if(ans == "n") {
+			// [first,mid], here mid is the included in the range because the way he the question goes.
+			last = mid ;
+		}
+
+	} 
+	cout << "Your number is " << first;
+
+} 
+
